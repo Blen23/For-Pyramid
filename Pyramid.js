f2fd4cf69df1cde5921a94127a0b9c83df57e947
Blen23 <blenfesehaye@gmail.com>
2021-07-18 03:13:26 -0700

    change to html structure to make it for new lines to be displayed

--- Pyramid.js
+++ Pyramid.js
@@ -2,6 +2,10 @@
  var linebreak = '\n';
  for(var i=0; i <row; i++)
  {
+    for(var k=0; k<=row; k++)
+    {
+        console.log('\t');
+    }
      for(var j=0; j<=i; j++)
      {
          console.log("* ");
